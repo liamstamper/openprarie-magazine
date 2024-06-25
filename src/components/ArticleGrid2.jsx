d263655325abdb3437498394cc68f8e7accc3ddc
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-06-25 11:28:42 -0500

    Created ArticleGrid2

---
+++ src/components/ArticleGrid2.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function ArticleGrid2({ title, image, author, date, content, link }) {
+  return (
+    <section className="py-6">
+      <div className="container max-w-6xl p-6 pt-10 mx-auto space-y-6 sm:space-y-12">
+        <a
+          rel="noopener noreferrer"
+          href={link}
+          className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 border-solid border border-grey-50"
+        >
+          <img
+            src={image}
+            alt=""
+            className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-300"
+          />
+          <div className="p-6 space-y-2 lg:col-span-5">
+            <h3 className="text-2xl font-semibold sm:text-4xl ">{title}</h3>
+            <div className="text-xs tracking-wider font-semibold text-blue-500">
+              By {author}
+            </div>
+            <div>
+              {content}
+              <span className="font-bold hover:underline">
+                &nbsp;Continue Reading &gt;
+              </span>
+            </div>
+
+            <div className="text-xs text-gray-600">{date}</div>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default ArticleGrid2;
