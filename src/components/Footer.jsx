d86002bfcd6e34fcf51d3c426256d6502b52a1c4
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-01 13:27:44 -0500

    updated footer link styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,12 @@ const Footer = () => {
     <footer className="bg-white text-gray-900">
       <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-600">
         <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact Us</Link>
+          <Link className="text-xs hover:underline" to="/about">
+            About
+          </Link>
+          <Link className="text-xs hover:underline" to="/contact">
+            Contact Us
+          </Link>
         </ul>
         <div className="flex flex-col justify-center pt-6 lg:pt-0">
           <div className="flex justify-center space-x-4">
