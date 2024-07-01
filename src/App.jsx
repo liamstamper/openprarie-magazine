fca31d52d9bde8ddd6ac824c71f7daa4d67a4f0e
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-01 08:37:33 -0500

    created section routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./HomePage";
+import SectionPage from "./components/SectionPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/section/:sectionId" element={<SectionPage />} />
       </Routes>
       <Footer />
     </Router>
