c47a30b90a0065c33489f474c7ab42369c38fe0e
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-04 14:14:19 -0500

    updated to use articledata.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import ArticleGrid1 from "./components/ArticleGrid1";
 import ArticleGrid2 from "./components/ArticleGrid2";
 import ArticleGrid3 from "./components/ArticleGrid3";
-
 import ViewMore from "./components/ViewMore";
-
-import React from "react";
+import ArticlesData from "./components/ArticlesData";
 
 const HomePage = () => {
   return (
@@ -13,27 +13,16 @@ const HomePage = () => {
         title="Ei delenit sensibus liberavisse pri. Est in graece fuisset, eos affert putent doctus id."
         date="February 19, 2021"
         image="null"
-        author="David Doe"
+        author="Henry Romain"
         content="Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est i Ei delenit sensibus liberavisse pri. Quod suscipit no nam.Est i Ei delenit sensibus liberavisse pri. Quod suscipit no nam."
         link="null"
       />
       <ArticleGrid1
-        sectionTitle="Ei delenit sensibus liberavisse pri. Est in graece fuisset, eos affert putent doctus id."
-        sectionSubtitle="Ei delenit sensibus liberavisse pri. Est in graece fuisset, eos affert putent doctus id."
-        articleTitles={[
-          { title: "#React", url: "https://example.com/search?tag=React" },
-          { title: "#WebDev", url: "https://example.com/search?tag=WebDev" },
-          { title: "#WebDev", url: "https://example.com/search?tag=WebDev" },
-          {
-            title: "#Tailwi",
-            url: "https://example.com/search?ta",
-          },
-        ]}
-        images={["John Smith", "John Smith", "John Smith", "John Smith"]}
-        authors={["John Smith", "John Smith", "John Smith", "John Smith"]}
-        dates={["June 1, 2024", "June 1, 2024", "June 1, 2024", "June 1, 2024"]}
+        articles={ArticlesData.slice(0, 4)}
+        sectionTitle="Featured Articles"
+        sectionSubtitle="Highting our top articles this month"
       />
-      <ViewMore section="web" url="null" />
+      <ViewMore section="Recent" url="null" />
     </div>
   );
 };
