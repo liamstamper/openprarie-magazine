8532cba56c36253f0f3224cd3804a7bb80b1350a
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-04 14:14:01 -0500

    updated to use articledata.jsx

--- src/components/ArticleGrid1.jsx
+++ src/components/ArticleGrid1.jsx
@@ -1,13 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function ArticleGrid1({
-  sectionTitle,
-  sectionSubtitle,
-  articleTitles,
-  images,
-  authors,
-  dates,
-}) {
+function ArticleGrid1({ articles, sectionTitle, sectionSubtitle }) {
   return (
     <section className="py-6 sm:py-12">
       <div className="container p-6 mx-auto space-y-8">
@@ -16,138 +10,33 @@ function ArticleGrid1({
           <p className="font-serif text-sm text-gray-600">{sectionSubtitle}</p>
         </div>
         <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
-          <article className="flex flex-col border-solid border border-grey-50">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              aria-label="Te nulla oportere reprimique his dolorum"
+          {articles.map((article, index) => (
+            <article
+              key={index}
+              className="flex flex-col border-solid border border-grey-50"
             >
-              <img
-                alt=""
-                className="object-cover w-full h-52 bg-gray-300"
-                src={images[0]}
-              />
-            </a>
-            <div className="flex flex-col flex-1 p-6">
-              <a
-                href={articleTitles[0].url}
-                key={0}
-                className="flex-1 py-2 text-lg font-semibold hover:underline leading-snug"
-              >
-                {articleTitles[0].title}
-              </a>
-
-              <div
-                rel="noopener noreferrer"
-                href="#"
-                className="text-xs tracking-wider font-semibold text-blue-500"
-              >
-                By {authors[0]}
+              <Link to={`/article/${article.id}`}>
+                <img
+                  className="object-cover w-full h-52 bg-gray-300"
+                  src={article.imageUrl}
+                />
+              </Link>
+              <div className="flex flex-col flex-1 p-6">
+                <Link
+                  to={`/article/${article.id}`}
+                  className="flex-1 py-2 text-lg font-semibold hover:underline leading-snug"
+                >
+                  {article.title}
+                </Link>
+                <div className="text-xs tracking-wider font-semibold text-blue-500">
+                  By {article.author}
+                </div>
+                <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs text-gray-600">
+                  <span>{article.date}</span>
+                </div>
               </div>
-              <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs text-gray-600">
-                <span>{dates[0]}</span>
-              </div>
-            </div>
-          </article>
-          <article className="flex flex-col border-solid border border-grey-50">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              aria-label="Te nulla oportere reprimique his dolorum"
-            >
-              <img
-                alt=""
-                className="object-cover w-full h-52 bg-gray-300"
-                src={images[1]}
-              />
-            </a>
-            <div className="flex flex-col flex-1 p-6">
-              <a
-                href={articleTitles[1].url}
-                key={0}
-                className="flex-1 py-2 text-lg font-semibold hover:underline leading-snug"
-              >
-                {articleTitles[1].title}
-              </a>
-
-              <div
-                rel="noopener noreferrer"
-                href="#"
-                className="text-xs tracking-wider font-semibold text-blue-500"
-              >
-                By {authors[1]}
-              </div>
-              <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs text-gray-600">
-                <span>{dates[1]}</span>
-              </div>
-            </div>
-          </article>
-          <article className="flex flex-col border-solid border border-grey-50">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              aria-label="Te nulla oportere reprimique his dolorum"
-            >
-              <img
-                alt=""
-                className="object-cover w-full h-52 bg-gray-300"
-                src={images[2]}
-              />
-            </a>
-            <div className="flex flex-col flex-1 p-6">
-              <a
-                href={articleTitles[2].url}
-                key={0}
-                className="flex-1 py-2 text-lg font-semibold hover:underline leading-snug"
-              >
-                {articleTitles[2].title}
-              </a>
-
-              <div
-                rel="noopener noreferrer"
-                href="#"
-                className="text-xs tracking-wider font-semibold text-blue-500"
-              >
-                By {authors[2]}
-              </div>
-              <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs text-gray-600">
-                <span>{dates[2]}</span>
-              </div>
-            </div>
-          </article>
-          <article className="flex flex-col border-solid border border-grey-50">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              aria-label="Te nulla oportere reprimique his dolorum"
-            >
-              <img
-                alt=""
-                className="object-cover w-full h-52 bg-gray-300"
-                src={images[3]}
-              />
-            </a>
-            <div className="flex flex-col flex-1 p-6">
-              <a
-                href={articleTitles[3].url}
-                key={0}
-                className="flex-1 py-2 text-lg font-semibold hover:underline leading-snug"
-              >
-                {articleTitles[3].title}
-              </a>
-
-              <div
-                rel="noopener noreferrer"
-                href="#"
-                className="text-xs tracking-wider font-semibold text-blue-500"
-              >
-                By {authors[3]}
-              </div>
-              <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs text-gray-600">
-                <span>{dates[3]}</span>
-              </div>
-            </div>
-          </article>
+            </article>
+          ))}
         </div>
       </div>
     </section>
