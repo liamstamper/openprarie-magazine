f3cf81b24989c87dd6610f756cfaf2dcb332518d
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-05 10:21:00 -0500

    fixed about and contact us resizing for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,20 @@ const Footer = () => {
   return (
     <footer className="bg-white text-gray-900">
       <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-600">
-        <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
-          <Link className="font-semibold text-xs hover:underline" to="/about">
-            About
-          </Link>
-          <Link className="font-semibold text-xs hover:underline" to="/contact">
-            Contact Us
-          </Link>
+        <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:space-x-4 lg:flex-1 lg:justify-start">
+          <li className="mb-2 sm:mb-0 sm:mr-4">
+            <Link className="font-semibold text-xs hover:underline" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="font-semibold text-xs hover:underline"
+              to="/contact"
+            >
+              Contact Us
+            </Link>
+          </li>
         </ul>
         <div className="flex flex-col justify-center pt-6 lg:pt-0">
           <div className="flex justify-center space-x-4">
