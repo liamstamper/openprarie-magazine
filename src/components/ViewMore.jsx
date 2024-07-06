e2b94216ba9db2486198e860647780ae0d2c6896
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-06 12:40:18 -0500

    refactor viewmore to link to section with title

--- src/components/ViewMore.jsx
+++ src/components/ViewMore.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function ViewMore({ section, url }) {
+function ViewMore({ section }) {
   return (
     <div className="flex justify-center">
-      <a
-        href={url}
+      <Link
+        to={`/section/${section}`} // Corrected to use template literals for dynamic URL
         className="px-6 py-3 text-xs font-semibold rounded-md hover:underline text-black"
       >
         View more from {section} <span className="text-xs">&gt;&gt;</span>
-      </a>
+      </Link>
     </div>
   );
 }
