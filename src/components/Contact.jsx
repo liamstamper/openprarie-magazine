d81d29ad426b72ff4be7c0d5f968c6e8c53805b5
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-08-21 13:54:41 -0500

    contact sytle and text updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,23 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Contact = () => {
-  const submissionEmail = "submissions@yourmagazine.com";
   return (
-    <section className="container mx-auto flex flex-col items-center px-4 text-center md:py-10 md:px-10 lg:px-32 xl:max-w-5xl text-gray-800">
-      <h2 className="text-3xl font-bold">Contact Us</h2>
-      <div className="mt-4 text-lg">
-        <p className="mb-4">
-          For any inquiries we can be reached at{" "}
-          <a
-            href={`mailto:${submissionEmail}`}
-            className="text-blue-500 hover:underline"
-          >
-            {submissionEmail}
-          </a>
-          . For information on submissions please visit our{" "}
-          <Link className="underline" to="/submissions">
-            submissions page.
-          </Link>
+    <section className="text-gray-800">
+      <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+        <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">
+          Contact Us
+        </h2>
+        <p className=" container flex flex-col justify-center mx-auto text-center text-gray-600">
+          For any inquiries we can be reached at submissionbox187@gmail.com.
         </p>
       </div>
     </section>
